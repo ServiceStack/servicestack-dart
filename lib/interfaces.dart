814504d1af722379e10c563a258e8a93dc438589
Demis Bellot <demis.bellot@gmail.com>
2018-04-16 13:47:03 -0400

    Move IServiceClient to interfaces.dart

--- lib/interfaces.dart
+++ lib/interfaces.dart
@@ -53,3 +53,65 @@ class TypeContext
       ? new TypeContext(typeName: typeName, types: parentContext.types, childContext: childContext)
       : new TypeContext(typeName: typeName, types: childContext.types);
 }
+
+abstract class IServiceClient {
+  String baseUrl;
+  String bearerToken;
+  String refreshToken;
+  String userName;
+  String password;
+
+  Future<T> get<T>(IReturn<T> request, {Map<String, dynamic> args});
+
+  Future<Map<String, dynamic>> getUrl(String path, {Map<String, dynamic> args});
+
+  Future<T> getAs<T>(String path,
+      {Map<String, dynamic> args,
+      T responseAs});
+
+  Future<T> post<T>(IReturn<T> request, {dynamic body, Map<String, dynamic> args});
+
+  Future<Map<String, dynamic>> postToUrl(String path, dynamic body,
+      {Map<String, dynamic> args});
+
+  Future<T> postAs<T>(String path, dynamic body,
+      {Map<String, dynamic> args,
+      T responseAs});
+
+  Future<T> delete<T>(IReturn<T> request, {Map<String, dynamic> args});
+
+  Future<Map<String, dynamic>> deleteUrl(String path,
+      {Map<String, dynamic> args});
+
+  Future<T> deleteAs<T>(String path,
+      {Map<String, dynamic> args,
+      T responseAs});
+
+  Future<T> put<T>(IReturn<T> request, {dynamic body, Map<String, dynamic> args});
+
+  Future<Map<String, dynamic>> putToUrl(String path, dynamic body,
+      {Map<String, dynamic> args});
+
+  Future<T> putAs<T>(String path, dynamic body,
+      {Map<String, dynamic> args,
+      T responseAs});
+
+  Future<T> patch<T>(IReturn<T> request, {dynamic body, Map<String, dynamic> args});
+
+  Future<Map<String, dynamic>> patchToUrl(String path, dynamic body,
+      {Map<String, dynamic> args});
+
+  Future<T> patchAs<T>(String path, dynamic body,
+      {Map<String, dynamic> args,
+      T responseAs});
+
+  Future<T> send<T>(IReturn<T> request,
+      {String method,
+      Map<String, dynamic> args,
+      T responseAs});
+}
+
+class TypeAs {
+  static String string = "";
+  static Uint8List bytes = new Uint8List(0);
+}
