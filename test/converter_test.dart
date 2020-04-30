9ca23b690d9764e9debce3715817629e12c76f9b
Dominik Heeb <dominik.heeb@zuehlke.com>
2020-04-30 17:47:03 +0200

    added tests for dateTimeConverter bugs with 8601 strings

--- test/converter_test.dart
+++ test/converter_test.dart
@@ -5,8 +5,24 @@ main() {
   test('Can parse ISO 8601 DateTime', () {
     var converter = new DateTimeConverter();
 
-    var dateTime = converter.fromJson("2018-04-09T19:43:34.7405970", null);
+    DateTime dateTime = converter.fromJson("2018-04-09T19:43:34.7405970", null);
 
-    print(dateTime);
+    expect(dateTime.toIso8601String(), "2018-04-09T19:43:34.740597");
+  });
+
+  test('Can parse ISO 8601 DateTime with Timezone', () {
+    var converter = new DateTimeConverter();
+
+    DateTime dateTime = converter.fromJson("2018-04-09T19:43:34.740590+00:00", null);
+
+    expect("2018-04-09T19:43:34.740590+01:00", dateTime.toIso8601String());
+  });
+
+  test('Can parse ISO 8601 DateTime with UTC', () {
+    var converter = new DateTimeConverter();
+
+    DateTime dateTime = converter.fromJson("2018-04-09T19:43:34.7405901Z", null);
+
+    expect("2018-04-09T19:43:34.7405901Z", dateTime.toIso8601String());
   });
 }
