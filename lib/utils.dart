df58cda856fc7f0036f91bb9891166d38b9db48c
Demis Bellot <demis.bellot@gmail.com>
2021-06-29 14:48:26 +0800

    move ext method to utils

--- lib/utils.dart
+++ lib/utils.dart
@@ -360,3 +360,14 @@ String fromGuid(String guid) => guid;
 // From Guid string to .NET Guid
 String toGuid(String guid) => guid;
 
+
+//https://github.com/dart-lang/collection/blob/master/lib/src/iterable_extensions.dart
+extension IterableExtensions<T> on Iterable<T> {
+  /// The first element satisfying [test], or `null` if there are none.
+  T? firstWhereOrNull(bool Function(T element) test) {
+    for (var element in this) {
+      if (test(element)) return element;
+    }
+    return null;
+  }
+}
